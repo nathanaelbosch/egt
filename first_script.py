9c6be690963f1547a1af0b7a78fa3dafaf51c876
Nathanael Bosch <nathanael.bosch@tum.de>
2018-06-16 12:31:47 +0200

    multiple particles, but functional code, but no/bad convergence

--- first_script.py
+++ first_script.py
@@ -1,26 +1,36 @@
+"""
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 _range = np.arange(-10, 10, 0.1)
 
-
+###############################################################################
+# Setup: We want to minimize the following function with EGT
+###############################################################################
 def f(x):
-    return x ** 2 + 0.5 * np.sin(10*x)
+    return x ** 2 #+ 0.5 * np.sin(10*x)
+# To plot:
+# plt.plot(_range, f(_range))
 
 
 # All available strategies:
 _stepsize = 0.01
 U = np.arange(-1+_stepsize, 1-_stepsize, _stepsize)
 
+
 # Initial mixed strategy - continuous:
 sigma = np.exp(-1/(1- (U**2)))
 sigma = sigma / np.sum(sigma)
 
 
-# Simple case: Two points, {0, 1}
-x1, sigma1 = 0, sigma
-x2, sigma2 = 1, sigma
+# Now with more than two points
+population = []
+population.append([0, sigma])
+population.append([1, sigma])
+population.append([-1, sigma])
+population.append([2, sigma])
 
 
 # J as described by Massimo
@@ -33,18 +43,31 @@ def J(x, u, x2):
         ((x-x2) ** alpha + (f(x) - f(x2)) ** alpha))
 
 
+###############################################################################
+# Here the actual simulation starts
+###############################################################################
+print('Start')
+print([y[0] for y in population])
+beta = 1
+gamma = 0.5
+delta_t = 0.1
 for i in range(10000):
+    if i % 1000 == 0:
+        print([y[0] for y in population])
     # No sum here as there are only two particles
-    delta1 = 1/1 * (J(x1, U, x2) - np.sum(J(x1, U, x2) * sigma1))
-    delta2 = 1/1 * (J(x2, U, x1) - np.sum(J(x2, U, x1) * sigma2))
+    deltas = []
+    for j, (x1, sigma1) in enumerate(population):
+        rescale_factor = np.sum([np.exp(- beta * f(x2)) for k, (x2, _) in enumerate(population) if k != j])
+        deltas.append(1/rescale_factor * np.sum(
+            [(J(x1, U, x2)-np.sum(J(x1, U, x2) * sigma1)) * np.exp(-beta*f(x2))
+             for k, (x2, _) in enumerate(population) if k != j], axis=0))
 
-    gamma = 1
-    delta_t = 0.1
     # Update sigmas
-    sigma1 = (1 + gamma * delta_t * delta1) * sigma1
-    sigma2 = (1 + gamma * delta_t * delta2) * sigma2
+    for j in range(len(population)):
+        population[j][1] *= (1 + gamma * delta_t * deltas[j])
+        population[j][1] /= np.sum(population[j][1])
+        population[j][0] += delta_t * np.random.choice(U, p=population[j][1])
 
-    # Update Locations
-    x1 = x1 + delta_t * np.random.choice(U, p=sigma1)
-    x2 = x2 + delta_t * np.random.choice(U, p=sigma2)
 
+# Locations:
+print([y[0] for y in population])
