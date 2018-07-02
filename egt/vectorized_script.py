764621ac227e0af13a6fdcfd76b87a6ed7b31c60
Nathanael Bosch <nathanael.bosch@tum.de>
2018-07-02 13:31:04 +0200

    More modular script and visualization
    Visualization now contains some 2d things
    
    Script is now more modular, to load even less things by default; Goal is
    to make the automated testing
    
    Test script was adapted as some structure changed

--- egt/vectorized_script.py
+++ egt/vectorized_script.py
@@ -29,10 +29,10 @@ gamma = 0.3
 delta_t = 0.1
 total_steps = 2*60*60
 # Multiple strategy update rounds per location update
-s_rounds = 2
+s_rounds = 1
 
 # Szenario 1: Minimum inside
-starting_locations = [-1, 0, 1, 3, 5]
+starting_locations = [-1, 0, 3, 5]
 
 # Szenario 2: Minimum outside
 # starting_locations = [1, 2, 3, 4]
@@ -55,30 +55,35 @@ def parse_args():
 
 
 ###############################################################################
-# Setup
+# Setup - Universal settings that are not affected by some parameter changes
 ###############################################################################
 # We want to minimize the following function with EGT
 def f(x):
     return x ** 2 + 0.5 * np.sin(30*x)
 
 
+N = len(starting_locations)
+
 # All available strategies:
 U = np.arange(-1, 1, _strategy_resolution)
 
 # Initial mixed strategy - continuous:
-sigma = np.exp(-1/(1-(U**2)))
+with np.errstate(divide='ignore'):
+    sigma = np.exp(-1/(1-(U**2)))
 sigma = sigma / np.sum(sigma)
 
-# Initial population: Now as a matrix. First col location, rest mixed strategy
-population = np.concatenate(
-    (np.array(starting_locations).reshape((len(starting_locations), 1)),
-     np.tile(sigma, (len(starting_locations), 1))),
-    axis=1)
-N = population.shape[0]
 
-# Object to save the whole process
-history = []
-history.append(population)
+def create_initial_population(starting_locations):
+    # Initial population: Now as a matrix. First col location, rest mixed strategy
+    population = np.concatenate(
+        (np.array(starting_locations).reshape((len(starting_locations), 1)),
+         np.tile(sigma, (len(starting_locations), 1))),
+        axis=1)
+
+    # Object to save the whole process
+    history = []
+    history.append(population)
+    return history
 
 
 def positive(x):
@@ -88,9 +93,11 @@ def positive(x):
 
 def J_original(x, u, x2):
     """J as described by Massimo"""
-    return np.exp(
-        -((u - positive(np.tanh(3*(f(x) - f(x2)))) * (x2 - x))**2) /
-        ((x-x2) ** alpha + (f(x) - f(x2)) ** alpha))
+    with np.errstate(divide='ignore'):
+        out = np.exp(
+            -((u - positive(np.tanh(3*(f(x) - f(x2)))) * (x2 - x))**2) /
+            ((x-x2) ** alpha + (f(x) - f(x2)) ** alpha))
+    return out
 
 
 def J(x, u, x2):
@@ -109,7 +116,6 @@ def J_vectorized(points):
 
     This one is actually used for computations.
 
-    It is a #U x N x N tensor now
     axis=0 the point to evaluate
     axis=1 the point to compare to
     axis=2 are the strategies
@@ -117,17 +123,27 @@ def J_vectorized(points):
     N = len(points)
     f_vals = f(points)
     f_diffs = np.tile(f_vals, reps=(N, 1)).T - np.tile(f_vals, reps=(N, 1))
+
+    # # take only the ones where i!=j
+    # rows = [[i]*(N-1) for i in range(N)]
+    # cols = [[j for j in range(N) if j!=i] for i in range(N)]
+    # f_diffs = f_diffs[rows, cols]
+
+    f_diffs = f_diffs[range(N), ]
     f_diffs_tanh = np.tanh(3*f_diffs)
     f_diffs_tanh_positive = np.where(
         f_diffs_tanh > 0,
         f_diffs_tanh,
         0)
     walk_dirs = np.tile(points, reps=(N, 1)) - np.tile(points, reps=(N, 1)).T
+    # walk_dirs = walk_dirs[rows, cols]
     walk_dirs_adj = f_diffs_tanh_positive * walk_dirs
     variance = walk_dirs ** alpha + f_diffs ** alpha
-    out = np.exp(
-        -1 * ((U.reshape(1, 1, len(U)) - walk_dirs_adj[:, :, None])**2) /
-        variance[:, :, None])
+
+    with np.errstate(divide='ignore'):
+        out = np.exp(
+            -1 * ((U.reshape(1, 1, len(U)) - walk_dirs_adj[:, :, None])**2) /
+            variance[:, :, None])
 
     out *= np.exp(beta * f_diffs)[:, :, None]
 
@@ -154,6 +170,7 @@ def main():
         np.random.seed(args.seed)
 
     print('Start')
+    history = create_initial_population(starting_locations)
     sim_bar = tqdm.tqdm(range(total_steps))
     sim_bar.set_description('Simulation')
     for i in sim_bar:
@@ -164,18 +181,19 @@ def main():
         for s in range(s_rounds):
             tot_J = J_vectorized(next_pop[:, 0])
             sum_i = tot_J.sum(axis=1)
-            mean_outcome = (sum_i * population[:, 1:]).sum(axis=1)
+            mean_outcome = (sum_i * current_pop[:, 1:]).sum(axis=1)
             delta = sum_i - mean_outcome[:, None]
             delta = np.sum(
                 tot_J - np.sum(
-                    tot_J * population[:, 1:][:, None, :], axis=2)[:, :, None],
+                    tot_J * current_pop[:, 1:][:, None, :],
+                    axis=2)[:, :, None],
                 axis=1)
             next_pop[:, 1:] *= (1 + gamma * delta_t * delta)
-            next_pop[:, 1:] /= next_pop[:, 1:].sum(axis=1)[:, None]
+            # next_pop[:, 1:] /= next_pop[:, 1:].sum(axis=1)[:, None]
 
-            # Location updates
-            for j in range(len(next_pop)):
-                next_pop[j, 0] += delta_t*np.random.choice(U, p=next_pop[j, 1:])
+        # Location updates
+        for j in range(len(next_pop)):
+            next_pop[j, 0] += delta_t*np.random.choice(U, p=next_pop[j, 1:])
 
         history.append(next_pop)
 
--- test_sample.py
+++ test_sample.py
@@ -2,15 +2,22 @@
 
 Single file, as there will probably not be much testing necessary"""
 import numpy as np
-from egt.vectorized_script import population, J, J_vectorized, N, U
+from egt.vectorized_script import *
 
 
 def test_J():
+    history = create_initial_population(starting_locations)
+    population = history[-1]
     points = population[:, 0]
     out = J_vectorized(population[:, 0])
     old = out.copy()
     for i in range(N):
-        for j in range(N):
+        for j in range(N-1):
+            # if i<=j:
+            #     for k in range(len(U)):
+            #         old[i, j, k] = J(
+            #             points[i], U[k], points[j+1])
+            # else:
             for k in range(len(U)):
                 old[i, j, k] = J(
                     points[i], U[k], points[j])
