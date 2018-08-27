4bff92f5f8225796741d30e33675a63c11244155
Nathanael Bosch <nathanael.bosch@tum.de>
2018-08-27 10:01:27 +0200

    better visualization

--- egt/convergence_analysis.py
+++ egt/convergence_analysis.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from .test_functions import convex_hull
+from .test_functions import convex_hull, simple_nonconvex_function
+from .visualisation import FIGSIZE
 
 
 def max_distances(history, f):
@@ -18,30 +19,36 @@ def max_distances(history, f):
 
 
 def visualize(history, f):
+    data = {}
     range = np.arange(-10, 10, 0.0001)
     values = f(range)
     min_val, min_loc = values.min(), values.argmin()
 
     location_hist, _ = zip(*history)
+    location_hist = list(location_hist)
+    while np.all(location_hist[-1] == location_hist[-2]):
+        location_hist.pop(-1)
     locs = np.array(location_hist).reshape(len(location_hist), -1)
 
     # Norm the values to have value 0 at the min
     mean_vals = f(locs).mean(axis=1) - min_val
-    convex_f = convex_hull(
-        f, plot_range=np.arange(locs.min(), locs.max(), 0.01))
-    mean_vals_convex = convex_f(locs).mean(axis=1) - min_val
+    data['mean_values'] = mean_vals
+
+    if f == simple_nonconvex_function:
+        convex_f = convex_hull(
+            f, plot_range=np.arange(locs.min(), locs.max(), 0.01))
+        mean_vals_convex = convex_f(locs).mean(axis=1) - min_val
+        data['mean_values_convex'] = mean_vals_convex
     # max_distances = locs.max(axis=1) - locs.min(axis=1)
 
-    data = {
-        'mean_values': mean_vals,
-        'mean_values_convex': mean_vals_convex
-    }
-
+    fig = plt.figure(figsize=FIGSIZE)
+    ax = fig.add_subplot(111)
     df = pd.DataFrame(data)
     ax = df.plot(
-        y=['mean_values', 'mean_values_convex'],
+        ax=ax,
         logy=True
     )
+    plt.tight_layout()
 
     # win_size = 100
     # df.mean_values.rolling(win_size, center=True).mean().plot(
