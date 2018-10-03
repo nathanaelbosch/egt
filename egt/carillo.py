2f5fac86f9b98154c491e256febda9c417e3382a
Nathanael Bosch <nathanael.bosch@tum.de>
2018-10-03 13:43:22 +0200

    Carillo also in a try catch block

--- egt/carillo.py
+++ egt/carillo.py
@@ -26,29 +26,34 @@ def minimize(f, initial_population, parameters):
 
     logging.info('Start simulation')
     sim_bar = tqdm.trange(max_iterations)
-    for i in sim_bar:
-        f_values = f(locations)
-        f_values_adjusted = f_values - np.min(f_values)
-        weights = np.exp(-beta * f_values_adjusted)
-        mean_loc = np.sum(locations * weights) / np.sum(weights)
+    try:
+        for i in sim_bar:
+            f_values = f(locations)
+            f_values_adjusted = f_values - np.min(f_values)
+            weights = np.exp(-beta * f_values_adjusted)
+            mean_loc = np.sum(locations * weights) / np.sum(weights)
 
-        directions = mean_loc - locations
+            directions = mean_loc - locations
 
-        u = np.random.normal(0, 1, size=locations.shape)
-        locations = (locations +
-                     stepsize*directions +
-                     gamma * np.sqrt(stepsize) * np.abs(directions) * u)
+            u = np.random.normal(0, 1, size=locations.shape)
+            locations = (locations +
+                         stepsize*directions +
+                         gamma * np.sqrt(stepsize) * np.abs(directions) * u)
 
-        history.append((locations.copy(), None))
+            history.append((locations.copy(), None))
 
-        # Break condition for early stopping
-        max_dist_from_mean = np.max(np.abs(directions))
-        mean_value = np.mean(f(locations))
-        sim_bar.set_description((
-            '[Simulation] max_dist={:.2E} ' +
-            'mean_value={:.2f}').format(
-                max_dist_from_mean, mean_value))
-        if max_dist_from_mean < 1e-3:
-            break
+            # Break condition for early stopping
+            max_dist_from_mean = np.max(np.abs(directions))
+            mean_value = np.mean(f(locations))
+            sim_bar.set_description((
+                '[Simulation] max_dist={:.2E} ' +
+                'mean_value={:.2f}').format(
+                    max_dist_from_mean, mean_value))
+            if max_dist_from_mean < 1e-6:
+                break
+    except KeyboardInterrupt:
+        print('Breaking the simulation. If you really want to exit press it again in the next 5 seconds')
+        import time
+        time.sleep(5)
 
     return history
